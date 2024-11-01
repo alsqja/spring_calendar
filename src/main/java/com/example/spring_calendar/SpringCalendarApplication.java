ef810cdbaeee757c58d91752cf711c4d360aa792
alsqja <alsqha2626@gmail.com>
2024-11-01 21:25:54 +0900

    first commit

---
+++ src/main/java/com/example/spring_calendar/SpringCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCalendarApplication.class, args);
+    }
+
+}
